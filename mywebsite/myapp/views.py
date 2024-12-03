5b668c933ea76c525b705d60ef77c308468e5b2d
Dimitris Kopsidas <jimkopsidas@hotmail.com>
2024-12-03 22:18:16 +0200

    First setup

---
+++ mywebsite/myapp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse #gpt
+
+def light_view(request):#gpt
+    return HttpResponse("Let there be light")#gpt
+
+
